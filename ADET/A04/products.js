c221e53a8c34eb4010e2759c580913a82a7b62ff
Mechel Dela pena <zielcocloei@gmail.com>
2025-05-05 18:27:51 +0800

    POS

---
+++ ADET/A04/products.js
@@ -0,0 +1,18 @@
+const products = [
+  { id: 1, name: "50 pcs of Red Roses", price: 250.00, category: "Standing Bouquets", image: "assets/images/flowers/50redroses.jpg" },
+  { id: 2, name: "100 pcs of White Roses", price: 600.00, category: "Standing Bouquets", image: "assets/images/flowers/100pcswhiteroses .jpg" },
+  { id: 3, name: "15 pcs of Pink Roses", price: 120.00, category: "Standing Bouquets", image: "assets/images/flowers/15pcspinkroses .jpg" },
+  { id: 4, name: "Mixed Flowers", price: 200.00, category: "Standing Bouquets", image: "assets/images/flowers/mixedflowers .jpg" },
+  { id: 5, name: "Baby Blue Flowers", price: 170.00, category: "Standing Bouquets", image: "assets/images/flowers/babyblue .jpg" },
+  { id: 6, name: "Red Necklace", price: 120.00, category: "Flower Necklace", image: "assets/images/flowers/rednecklace .jpg" },
+  { id: 7, name: "White Necklace", price: 150.00, category: "Flower Necklace", image: "assets/images/flowers/whitenecklace .jpg" },
+  { id: 8, name: "Yellow and White Necklace", price: 100.00, category: "Flower Necklace", image: "assets/images/flowers/white&yellownecklace .jpg" },
+  { id: 9, name: "Yellow Chrysanthemums  with eucalyptus", price: 190.00, category: "Vase Arrangements", image: "assets/images/flowers/yellow2.jpg" },
+  { id: 10, name: "White and Pink", price: 450.00, category: "Vase Arrangements", image: "assets/images/flowers/white&pink .jpg" },
+  { id: 11, name: "Yellow Roses", price: 800.00, category: "Vase Arrangements", image: "assets/images/flowers/yellow1.jpg" },
+  { id: 12, name: "Mixed Vibrant", price: 300.00, category: "Vase Arrangements", image: "assets/images/flowers/mixedvibrant .jpg" },
+  { id: 13, name: "White Forex Box", price: 150.00, category: "Box Arrangements", image: "assets/images/flowers/whiteforex .jpg" },
+  { id: 14, name: "Blue Flowers", price: 160.00, category: "Box Arrangements", image: "assets/images/flowers/blueflowers .jpg" },
+  { id: 15, name: "Pink Flowers", price: 170.00, category: "Box Arrangements", image: "assets/images/flowers/pink white box .jpg" },
+  
+];
